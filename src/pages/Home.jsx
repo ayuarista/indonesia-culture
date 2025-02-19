847d213f16175acd433ebd1e0d3dc93fa39abc11
ayuarista <aristaadewi8@gmail.com>
2025-02-19 11:35:32 +0800

    initialize vite, react, and tailwind

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-white font-bold bg-amber-300">Welcome to the Home Page</h1>
+            <p>This is the default page for your website.</p>
+            <button>Hello WOrld</button>
+        </div>
+    );
+}
+
+export default Home;
