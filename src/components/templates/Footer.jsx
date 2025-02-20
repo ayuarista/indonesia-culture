c1cf92366caa1cb84eca5f1ad1fa851a192b3a2d
ayuarista <aristaadewi8@gmail.com>
2025-02-20 15:05:28 +0800

    add assets for footer

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -1,7 +1,9 @@
+import Gelombang from "/assets/footer/gelombang.png";
 const Footer = () => {
     return(
-        <>
-        </>
+        <footer className="bg-gradient-to-t from-[#430304] to-[#B20607]">
+            <img src={Gelombang} alt="" />
+        </footer>
     )
 }
 
