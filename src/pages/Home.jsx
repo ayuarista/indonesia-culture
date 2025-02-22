cee7f1bd53ec24fd782b114d65690424ad403335
ayuarista <aristaadewi8@gmail.com>
2025-02-22 19:22:31 +0800

    create jumbotron and add assets for jumbotron

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import Card from "../components/Card/Card";
+import gelombang from "/assets/home/gelombang-home.svg"
 const Home = () => {
     return (
         <div className="pt-16">
-            <h1 className="text-black font-bold bg-amber-300">Welcome to the Home Page</h1>
-            <p>This is the default page for your website.</p>
-            <Card/>
+           <div className="relative h-screen">
+            <div className="bg-gradient-to-b from-[#430304] to-[#A90809]  min-h-[40vh]">
+                <h1 className="font-bold text-6xl text-center absolute top-32">Preserve Culture, 
+                Protect National Identity</h1>
+            </div>
+            <img src={gelombang} alt="" className="absolute -mt-1"/>
+           </div>
         </div>
     );
 }
