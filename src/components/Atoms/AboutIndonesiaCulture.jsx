98b7653e0b005c6ccb08a0906194ce1edefc25a9
ayuarista <aristaadewi8@gmail.com>
2025-02-22 21:56:48 +0800

    create component

--- src/components/Atoms/AboutIndonesiaCulture.jsx
+++ src/components/Atoms/AboutIndonesiaCulture.jsx
@@ -4,7 +4,7 @@ const AboutIndonesiaCulture = () => {
         <div className="text-black">
             <div className="flex flex-col gap-1">
             <h1 className="font-bold text-2xl">What do you know about</h1>
-            <h1 className="font-bold text-3xl">Indonesian Culture?</h1>
+            <h1 className="font-bold text-3xl"> Culture?</h1>
             </div>
             <p className="font-medium mt-3 max-w-[35rem] text-justify">Indonesian culture is a rich and diverse collection of traditions, arts, customs, and values shaped by the country’s long history and the influence of indigenous, Hindu-Buddhist, Islamic, and colonial cultures.</p>
             <div className="mt-5">
