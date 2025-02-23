36dadf6f9d0f336918a63d4b6f164077c6106f1c
ayuarista <aristaadewi8@gmail.com>
2025-02-23 22:16:27 +0800

    Refactor About Indonesia Culture and About Rekor Muri components for improved styling and layout

--- src/components/Atoms/AboutRekorMuri.jsx
+++ src/components/Atoms/AboutRekorMuri.jsx
@@ -3,12 +3,12 @@ import ViewAllNews from "./ViewAllNews"
 const AboutRekorMuri = () => {
     return (
         <div>
-            <div className="flex items-center">
-                <h1 className="bg-gradient-to-r text-4xl from-0% from-[#490303] to-40% to-[#AF0808]">Rekor Muri Indonesia</h1>
+            <div className="flex items-center gap-2">
+                <h1 className="bg-gradient-to-r font-bold text-4xl from-0% from-[#490303] to-40% to-[#AF0808]">Rekor Muri Indonesia</h1>
                 <img src="" alt="" />
             </div>
             <div>
-            <p className="font-medium">The Indonesia World Records Museum is a museum located in Semarang, Central Java, Indonesia. A collection of Indonesian records is presented in this museum.</p>
+            <p className="font-medium mt-3 text-justify">The Indonesia World Records Museum is a museum located in Semarang, Central Java, Indonesia. A collection of Indonesian records is presented in this museum.</p>
             </div>
             <div className="mt-5">
                 <ViewAllNews/>
