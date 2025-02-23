0eaa034d16bf4da0f014b08eb2f5c9c16f4bc291
ayuarista <aristaadewi8@gmail.com>
2025-02-23 22:32:44 +0800

    create card rekor muri

--- src/components/Card/CardRekorMuri.jsx
+++ src/components/Card/CardRekorMuri.jsx
@@ -2,8 +2,9 @@ const CardRekorMuri = () => {
     return(
         <>
             <div className="relative">
+                <div className="absolute right-0 bottom-2 p-10 w-80 bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full"></div>
                 <div className="absolute right-0 top-0 p-7 w-64 bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full"></div>
-                <div className="absolute right-0 bottom-2 p-10 w-72 bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full"></div>
+                <img src="" alt="" />
             </div>
         </>
 
