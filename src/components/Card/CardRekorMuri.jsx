1f01aaa01a3fc09bee150300d848d647ee3a3773
ayuarista <aristaadewi8@gmail.com>
2025-02-23 22:06:08 +0800

    Create Card Rekor Muri

---
+++ src/components/Card/CardRekorMuri.jsx
@@ -0,0 +1,11 @@
+const CardRekorMuri = () => {
+    return(
+        <>
+            <div className="relative">
+                <div className="absolute p-5 h-64 bg-gradient-to-r from-[#430304] to-[#A90809]"></div>
+            </div>
+        </>
+
+    )
+}
+export default CardRekorMuri;
