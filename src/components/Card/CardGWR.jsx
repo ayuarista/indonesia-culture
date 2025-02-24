4d0fcdaefaad2ce9a2e91801356fb637480f4636
ayuarista <aristaadewi8@gmail.com>
2025-02-24 14:51:17 +0800

    Create data GWR

--- src/components/Card/CardGWR.jsx
+++ src/components/Card/CardGWR.jsx
@@ -1,20 +1,20 @@
-const CardGWR = () => {
+const CardGWR = (props) => {
     return(
         <div className="relative max-w-sm rounded-lg shadow-xl">
             <div className="relative">
-            <img src="https://awsimages.detik.net.id/community/media/visual/2023/08/06/kemendikbudristek_169.jpeg?w=600&q=90" 
-            alt="" 
+            <img src={props.image} 
+            alt={props.title}
             className="w-full object-cover rounded-t-[21px]"
             />
             <h1 className="absolute bottom-2 font-bold text-xl">
-                Largest Angklung Ensemble
+                {props.title}
             </h1>
             </div>
             <div className="absolute top-0 left-0 bg-white text-[#AF0808] font-bold px-3 py-2 border border-gray-300 rounded-tl-full rounded-br-full rounded-tr-full">
-                2023
+                {props.year}
             </div>
             <div className="p-4">
-                <p className="text-justify font-medium text-[15px] text-black">Largest Angklung Strings (2023) - 15,110 people played angklung simultaneously in Jakarta.</p>
+                <p className="text-justify font-medium text-[15px] text-black">{props.desc}</p>
             </div>
         </div>
     )
---
+++ src/components/data/Card/DataCardGWR.jsx
@@ -0,0 +1,25 @@
+const DataCardGWR = [
+    {
+        id: 1,
+        image: "https://awsimages.detik.net.id/community/media/visual/2023/08/06/kemendikbudristek_169.jpeg?w=600&q=90",
+        year: 2023,
+        title: "Largest Angklung Ensemble",
+        desc: "Largest Angklung Strings (2023) – 15,110 people played angklung simultaneously in Jakarta."
+    },
+    {
+        id: 2,
+        image: "https://cdn1.katadata.co.id/media/images/temp/2021/12/02/Gambar_Tari_Jaipong-2021_12_02-12_33_00_06b1338ba02add2b5d2da67663b19ebe.jpg",
+        year: 2018,
+        title: "The Most Jaipong Dance",
+        desc: "The mass performance of Jaipong Dance, West Java, was recognized by Guinness World Records - 10,000 participants."
+    },
+    {
+        id: 3,
+        image: "https://asset-a.grid.id/crop/0x0:0x0/x/photo/2018/08/07/3088467194.jpeg",
+        year: 2018,
+        title: "The Most Poco-Poco Dance",
+        desc: "Indonesia holds the record for the largest Poco-Poco dance, an Indonesian dance, with more than 65,000 participants."
+    },
+]
+
+export default DataCardGWR;
