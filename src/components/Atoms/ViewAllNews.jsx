83c3b90a8144286945a025bf1f0f2be468ffa612
ayuarista <aristaadewi8@gmail.com>
2025-02-24 09:36:58 +0800

    Fixing Style for component Rekor Muri

--- src/components/Atoms/ViewAllNews.jsx
+++ src/components/Atoms/ViewAllNews.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 const ViewAllNews = () => {
     return(
-        <button className="px-6 py-2 rounded-full border  border-[#B20607]">
+        <button className="px-5 py-2 rounded-full border  border-[#B20607]">
         <Link to="/article">
-            <p className="text-[#B20607] font-semibold">View All News</p>
+            <p className="text-[#B20607] font-semibold text-[14px]">View All News</p>
         </Link>
         </button>
     )
--- src/components/Card/CardRekorMuri.jsx
+++ src/components/Card/CardRekorMuri.jsx
@@ -2,10 +2,11 @@ const CardRekorMuri = () => {
     return(
         <>
             <div className="relative">
-                <div className="absolute right-0 bottom-2 p-14 w-96 bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full"></div>
-                <div className="absolute right-0 top-0 p-9 w-72 bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full"></div>
-                <div className="absolute w-52 bottom-0 right-0">
-                    <img src="https://plus.unsplash.com/premium_photo-1691547067560-f40ee5d0b7d5?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full object-cover"/>
+                <div className="absolute right-0 bottom-2 p-14 w-[28rem] bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full"></div>
+                <div className="absolute right-0 top-0 p-10 w-96 bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full">
+                <div className="absolute w-52 h-44 bottom-0 right-0">
+                    <img src="https://plus.unsplash.com/premium_photo-1691547067560-f40ee5d0b7d5?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className=" border-white h-44 border-[5px] w-full object-cover rounded-tl-full rounded-br-full"/>
+                </div>
                 </div>
             </div>
         </>
