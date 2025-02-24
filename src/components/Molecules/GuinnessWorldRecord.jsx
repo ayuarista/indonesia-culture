047dd54907d7ccb7c3b5e744050167c433e960c9
ayuarista <aristaadewi8@gmail.com>
2025-02-24 14:54:03 +0800

    Mapping data GuinnessWorldRecord

---
+++ src/components/Molecules/GuinnessWorldRecord.jsx
@@ -0,0 +1,11 @@
+import CardGWR from "../Card/CardGWR";
+import DataCardGWR from "../data/card/DataCardGWR";
+const GuinnessWorldRecord = () => {
+    return (
+        <div className="grid grid-cols-3">
+            
+        </div>
+    )
+}
+
+export default GuinnessWorldRecord;
