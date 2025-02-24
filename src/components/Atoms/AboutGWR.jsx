c0fc81cbc79ff9ff9d01b1bcf5b946abf57b978b
ayuarista <aristaadewi8@gmail.com>
2025-02-24 14:09:52 +0800

    create components about GWR

--- src/components/Atoms/AboutGWR.jsx
+++ src/components/Atoms/AboutGWR.jsx
@@ -1,6 +1,10 @@
 const AboutGWR = () => {
     return(
-        <div></div>
+        <div className="text-center flex-col gap-2">
+            <p className="text-[#A90809] font-medium">Indonesian International Cultural Awards</p>
+            <h1 className="bg-gradient-to-r from-0% from-[#430304] to-40% to-[#A90809] text-transparent bg-clip-text font-bold text-4xl">Guinness World Records (GWR)</h1>
+            <p className="font-medium">Guinness World Records (GWR) is a globally recognized organization that documents and verifies world records in various fields, including human achievements, natural phenomena, and unique events.</p>
+        </div>
     )
 }
 
