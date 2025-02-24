7b2aff02bd0f7dd8c224ae84c1c08aa5450fb6e8
ayuarista <aristaadewi8@gmail.com>
2025-02-24 14:34:27 +0800

    create Components Card GWR

--- src/components/Card/CardGWR.jsx
+++ src/components/Card/CardGWR.jsx
@@ -1,7 +1,19 @@
 const cardGWR = () => {
     return(
-        <div>
-
+        <div className="relative max-w-md rounded-lg shadow-xl">
+            <img src="https://images.unsplash.com/photo-1544180994-c8c0ecf8eef0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmFsaSUyMGRhbmNlfGVufDB8fDB8fHww" 
+            alt="" 
+            className="w-full object-cover"
+            />
+            <h1 className="absolute bottom-0 font-bold text-xl">
+                Largest Angklung Ensemble
+            </h1>
+            <div className="absolute top-0 left-0 bg-white text-[#AF0808] font-bold border border-gray-300 rounded-tl-full rounded-br-full rounded-tr-full">
+                2023
+            </div>
+            <div className="p-4">
+                <p className="text-justify font-medium text-[15px]">Largest Angklung Strings (2023) - 15,110 people played angklung simultaneously in Jakarta.</p>
+            </div>
         </div>
     )
 }
