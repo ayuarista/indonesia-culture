9a266157fb126f2dfc38c7d1826d4563e95513df
ayuarista <aristaadewi8@gmail.com>
2025-02-26 14:33:39 +0800

    add icons views

---
+++ src/components/Card/CardNational.jsx
@@ -0,0 +1,18 @@
+
+const CardNational = () => {
+    return(
+        <div className="flex flex-col">
+            <div className="max-w-sm p-3 rounded-t-3xl border-gray-200">
+                <img src="" alt="" className="w-full object-cover rounded-2xl"/>
+                <div className="border-l-2 border-light-red px-2">
+                    <div className="flex items-center gap-2">
+                        <p className="text-gray-400 text-sm">Jakarta, 08 January, 2025</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardNational;
