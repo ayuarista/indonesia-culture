e498034bc54a83e4a4e2a7718fb06f0ba15de76e
ayuarista <aristaadewi8@gmail.com>
2025-02-26 09:54:39 +0800

    Fixing style Card Cultural Awards

--- src/components/Card/CardCulturalAwards.jsx
+++ src/components/Card/CardCulturalAwards.jsx
@@ -1,20 +1,20 @@
 const CardCulturalAwards = () => {
     return(
-        <div className="p-5 w-[30rem] h-44 bg-gradient-to-r from-[#430304] to-[#A90809] rounded-tl-full rounded-bl-full relative">
-        <div className="absolute -left-3 top-10">
+        <div className="p-5 w-96 h-44 bg-gradient-to-r from-[#430304] to-[#A90809] rounded-tl-full rounded-bl-full relative">
+        <div className="absolute -left-5 -top-10">
             <div className="w-32">
-            <img src="https://images.unsplash.com/photo-1599551634107-95ce4e763232?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGluZG9uZXNpYW4lMjBjdWx0dXJlfGVufDB8fDB8fHww" alt=""  className="w-full h-44 object-cover border-4 border-white rounded-b-full rounded-tl-full"
+            <img src="https://images.unsplash.com/photo-1599551634107-95ce4e763232?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGluZG9uZXNpYW4lMjBjdWx0dXJlfGVufDB8fDB8fHww" alt=""  className="w-full h-44 object-cover border-4 border-white rounded-bl-full rounded-t-full scale-x-[-1]"
             />
             </div>
         </div>
-        <div className="absolute left-36 bottom-10">
-            <div className="w-36">
-            <img src="https://images.unsplash.com/photo-1717924312594-31750e48a392?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRheWFrfGVufDB8fDB8fHww" alt="" className="w-full object-cover border-white border-4 rounded-full"/>
+        <div className="absolute left-32 -top-12">
+            <div className="w-48">
+            <img src="https://images.unsplash.com/photo-1717924312594-31750e48a392?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRheWFrfGVufDB8fDB8fHww" alt="" className="w-full object-cover border-white border-4 h-36 object-top rounded-b-3xl rounded-tr-3xl scale-x-[-1]"/>
             </div>
         </div>
-        <div className="absolute right-14 bottom-0">
-            <div className="w-32">
-            <img src="https://images.unsplash.com/photo-1698824554110-7187e3068031?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTkwfHxpbmRvbmVzaWFuJTIwY3VsdHVyZXxlbnwwfHwwfHx8MA%3D%3D" alt="" className="w-full h-56 object-cover border-white border-4 rounded-full"/>
+        <div className="absolute left-32 -bottom-14">
+            <div className="w-44">
+            <img src="https://images.unsplash.com/photo-1698824554110-7187e3068031?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTkwfHxpbmRvbmVzaWFuJTIwY3VsdHVyZXxlbnwwfHwwfHx8MA%3D%3D" alt="" className="w-full object-cover border-white border-4 rounded-tl-3xl rounded-br-3xl scale-x-[-1]"/>
             </div>
         </div>
     </div>
