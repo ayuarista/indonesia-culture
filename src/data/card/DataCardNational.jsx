1698734db4eaa9a4ff9da813fd545706613145ef
ayuarista <aristaadewi8@gmail.com>
2025-02-27 10:42:53 +0800

    create data card national

--- src/data/card/DataCardNational.jsx
+++ src/data/card/DataCardNational.jsx
@@ -1,7 +1,88 @@
 const DataCardNational = [
     {
-
-    }
+        id: 1,
+        image: "https://muri.org/assets/website/images/record/12084-1736936007.png",
+        date: "Jakarta, 08 January, 2025",
+        views: "566k",
+        title: "The First Person to Translate the Deuterocanonical Books and Part of the New Testament into Macapat.", 
+        category: ['all', 'latest', 'popular']
+    },
+    {
+        id: 2,
+        image: "https://muri.org/assets/website/images/record/12078-1736931997.png",
+        date: "Dharmasraya, 03 January, 2025",
+        views: "322k",
+        title: "Talempong Pacik Performance by Most Women in Indonesia Cultural. ",
+        category: ['all', 'latest']
+    },
+    {
+        id: 3,
+        image: "https://muri.org/assets/website/images/record/12072-1736841313.png",
+        date: "Sasono Langen Budoyo TMII, 31 December, 2024",
+        views: "489k",
+        title: "Karmina Anthology Book for General and Student Level with the Most Authors",
+        category: ['all', 'latest', 'popular']
+    },
+    {
+        id: 4,
+        image: "https://muri.org/assets/website/images/record/12070-1735886429.png",
+        date: "Jakarta, 22 December, 2024",
+        views: "312k",
+        title: "Cross-Generation Kebaya Fashion Show with Most Participants in Indonesia.",
+        category: ['all', 'latest']
+    },
+    {
+        id: 5,
+        image: "https://muri.org/assets/website/images/record/12079-1735878272.png",
+        date: "Kupang, December 20, 2024",
+        views: "456k",
+        title: "Simultaneous Weaving Parade at the Most Locations and Motifs.",
+        category: ['all', 'latest', 'popular']
+    },
+    {
+        id: 6,
+        image: "https://muri.org/assets/website/images/record/12067-1736838460.png",
+        date: "Jakarta, 16 December, 2024",
+        views: "374k",
+        title: "Police Officer Who Founded the Most Bands and Created the Most Songs.",
+        category: ['all', 'popular', 'previous']
+    },
+    {
+        id: 7,
+        image: "https://muri.org/assets/website/images/record/11501-1710490679.png",
+        date: "Bali, January 10, 2024",
+        views: "360k",
+        title: "Dancing 'Joged Harmoni Bali' by the Most Female Dancers in Indonesia.",
+        category: ['all', 'previous', 'popular']
+    },
+    // {
+    //     id: 8,
+    //     image: "",
+    //     date: "",
+    //     views: "",
+    //     title: ""
+    // },
+    // {
+    //     id: 9,
+    //     image: "",
+    //     date: "",
+    //     views: "",
+    //     title: ""
+    // },
+    // {
+    //     id: 10,
+    //     image: "",
+    //     date: "",
+    //     views: "",
+    //     title: ""
+    // },
+    // {
+    //     id: 11,
+    //     image: "",
+    //     date: "",
+    //     views: "",
+    //     title: ""
+    // },
 ]
 
 export default DataCardNational
