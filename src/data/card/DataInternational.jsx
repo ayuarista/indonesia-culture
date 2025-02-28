6503d61767043de85be42ee997f8c846d2354905
ayuarista <aristaadewi8@gmail.com>
2025-02-28 11:03:47 +0800

    Create Data International

---
+++ src/data/card/DataInternational.jsx
@@ -0,0 +1,35 @@
+export const DataInternational = {
+    "UNESCO Recognition": [
+      {
+        id: 1,
+        title: "The Longest Batik Indonesia",
+        location: "Mojokerto, October 15, 2024",
+        description: "Indonesia’s longest batik, 5,053 meters, was made by 10,106 people in Mojokerto.",
+        image: "/images/batik.jpg"
+      },
+      {
+        id: 2,
+        title: "Largest Angklung Ensemble",
+        location: "Jakarta, August 5, 2023",
+        description: "Largest Angklung Strings – 15,110 people played angklung simultaneously in Jakarta.",
+        image: "/images/angklung.jpg"
+      }
+    ],
+    "Asia Awards (ABU)": [
+      {
+        id: 3,
+        title: "Radio Republic of Indonesia (RRI)",
+        location: "Indonesia, September 11, 1945",
+        description: "RRI provides news, culture, and education. It plays a role in international content exchange.",
+        image: "/images/rri.jpg"
+      },
+      {
+        id: 4,
+        title: "Republic of Indonesia Television (TVRI)",
+        location: "Indonesia, September 11, 1945",
+        description: "TVRI provides national news and plays a role in international content exchange.",
+        image: "/images/tvri.jpg"
+      }
+    ]
+  };
+  
