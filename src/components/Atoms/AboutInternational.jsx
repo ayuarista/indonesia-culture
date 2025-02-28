6b2c23f59c757b4a5ca1e5d062fc0e048e1edcc4
ayuarista <aristaadewi8@gmail.com>
2025-02-28 19:30:11 +0800

    add about International

---
+++ src/components/Atoms/AboutInternational.jsx
@@ -0,0 +1,9 @@
+const AboutInternational = () => {
+    return(
+        <div>
+            <h1 className="font-bold text-transparent bg-clip-text from-dark-red to-light-red">Indonesian International Cultural Awards</h1>
+            <p className="text-pretty font-medium mt-2">Indonesia's international cultural awards highlight its rich ancestral heritage. Recognition from UNESCO, Guinness World Records, ASEAN, and the World Crafts Council affirms its universal value and the need for preservation.</p>
+        </div>
+    )
+}
+export default AboutInternational
