2f86a7003e56e443578d9f217d9014fcf27ac1ae
ayuarista <aristaadewi8@gmail.com>
2025-03-01 15:11:44 +0800

    Add styling for responsive in phone

--- src/components/Card/CardUNESCO.jsx
+++ src/components/Card/CardUNESCO.jsx
@@ -1,25 +1,25 @@
 const CardUNESCO = () => {
     return (
-        <div className="relative bg-gradient-to-r from-[#430304] to-[#A90809] w-[80%] h-72 rounded-tr-xl rounded-br-full">
-            <div className="absolute top-2 left-24">
+        <div className="relative bg-gradient-to-r from-[#430304] to-[#A90809] w-[80%] h-72 rounded-tr-full rounded-br-full">
+            <div className="absolute top-2 left-16 lg:left-24">
                 <img src="https://images.unsplash.com/photo-1733049919526-3ccf0ae8f6ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGNhbmRpJTIwcHJhbWJhbmFufGVufDB8fDB8fHww"
                  alt="" 
                  className="w-56 h-28 object-cover rounded-tl-4xl rounded-tr-4xl rounded-bl-4xl border-[5px] border-white"
                  />
             </div>
-            <div className="absolute top-5 -right-20">
+            <div className="absolute top-5 -right-20 lg:-right-8">
                 <img src="https://images.unsplash.com/photo-1620409123739-3849e65df57a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2F5YW5nJTIwa3VsaXR8ZW58MHx8MHx8fDA%3D"
                  alt="" 
                  className="w-52 h-48 object-cover rounded-tl-4xl rounded-tr-4xl rounded-br-4xl border-[5px] border-white"
                  />
             </div>
-            <div className="absolute -bottom-[68px] left-24">
+            <div className="absolute -bottom-[68px] left-16 lg:left-24">
                 <img src="https://images.unsplash.com/photo-1586319826907-1ff4aadbaddc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmF0aWt8ZW58MHx8MHx8fDA%3D"
                  alt="" 
                  className="w-56 object-left h-56 object-cover rounded-tl-4xl rounded-br-4xl rounded-bl-4xl border-[5px] border-white"
                  />
             </div>
-            <div className="absolute -bottom-[60px] -right-16">
+            <div className="absolute -bottom-[60px] -right-16 lg:-right-4">
                 <img src="https://images.unsplash.com/photo-1556375413-f6cdc5e17398?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGJvcm9idWR1cnxlbnwwfHwwfHx8MA%3D%3D"
                  alt="" 
                  className="w-full h-32 object-cover rounded-tr-4xl rounded-br-4xl rounded-bl-4xl border-[5px] border-white"
