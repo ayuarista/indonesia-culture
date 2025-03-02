32aed771fcdc37c07261018f2aec51d7a1abf6d0
ayuarista <aristaadewi8@gmail.com>
2025-03-02 23:03:40 +0800

    add DataUNESCOHeritage data structure for UNESCO heritage details

---
+++ src/data/card/DataUNESCOHeritage.jsx
@@ -0,0 +1,38 @@
+export const DataUNESCOHeritage = [
+    {
+        id: 1,
+        image: "",
+        title: "Borobudur Temple",
+        info: "",
+        year: "",
+        desc: "",
+        date: ""
+    },
+    {
+        id: 2,
+        image: "",
+        title: "",
+        info: "",
+        year: "",
+        desc: "",
+        date: ""
+    },
+    {
+        id: 3,
+        image: "",
+        title: "",
+        info: "",
+        year: "",
+        desc: "",
+        date: ""
+    },
+    {
+        id: 4,
+        image: "",
+        title: "",
+        info: "",
+        year: "",
+        desc: "",
+        date: ""
+    },
+]
