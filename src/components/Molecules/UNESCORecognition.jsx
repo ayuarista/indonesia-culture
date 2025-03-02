599759fa86aa37b62cb3646429523c3c343c6fe1
ayuarista <aristaadewi8@gmail.com>
2025-03-02 19:31:44 +0800

    create unesco recognition

--- src/components/Molecules/UNESCORecognition.jsx
+++ src/components/Molecules/UNESCORecognition.jsx
@@ -1,6 +1,13 @@
+import HeaderInternationalAwards from "../Atoms/HeaderInternationalAwards"
+
 const UNESCORecognition = () => {
     return(
         <>
+        <HeaderInternationalAwards
+        // image={}
+        title={"UNESCO Recognition"}
+        desc={"UNESCO (United Nations Educational, Scientific and Cultural Organization). This recognition shows that Indonesian culture has historical, artistic, and traditional values ​​that are important to the world."}
+        />
         </>
     )
 }
