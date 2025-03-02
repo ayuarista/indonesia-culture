2710e9678c28801c39ab3b1f349c7713246f6d7a
ayuarista <aristaadewi8@gmail.com>
2025-03-02 19:51:07 +0800

    enhance UNESCOIntangibleCard component to accept image, title, year, and description props

--- src/components/Card/UNESCOIntangibleCard.jsx
+++ src/components/Card/UNESCOIntangibleCard.jsx
@@ -1,6 +1,24 @@
-const UNESCOIntangibleCard  = () => {
+const UNESCOIntangibleCard  = (image, title, year, desc) => {
     return(
-        <div></div>
+        <div className="relative max-w-md lg:max-w-sm rounded-lg shadow-xl">
+            <div className="relative">
+            <img src={image} 
+            alt={title}
+            className="w-full h-52 object-cover rounded-t-[21px]"
+            />
+            <div className="absolute bottom-0 left-0 right-0 h-44 hover:h-72  transition-all duration-300 ease-in-out bg-gradient-to-t from-red-900 via-black/10 to-transparent p-5 flex items-end">
+            <h1 className="absolute bottom-2 font-bold text-xl">
+                {title}
+            </h1>
+            </div>
+            </div>
+            <div className="absolute top-0 left-0 bg-white text-[#AF0808] font-bold px-3 py-2 border border-gray-300 rounded-tl-full rounded-br-full rounded-tr-full">
+                {year}
+            </div>
+            <div className="p-4">
+                <p className="text-justify font-medium text-[15px] text-black">{desc}</p>
+            </div>
+        </div>
     )
 }
 
