ddd9d7689626e7152eeaa4bcbcf5928a81fe6bcd
ayuarista <aristaadewi8@gmail.com>
2025-03-02 23:17:27 +0800

    refactor layout for UNESCO Recognition component to improve structure and readability

--- src/components/Molecules/UNESCORecognition.jsx
+++ src/components/Molecules/UNESCORecognition.jsx
@@ -13,8 +13,8 @@ const UNESCORecognition = () => {
         title={"UNESCO Recognition"}
         desc={"UNESCO (United Nations Educational, Scientific and Cultural Organization). This recognition shows that Indonesian culture has historical, artistic, and traditional values ​​that are important to the world."}
         />
-       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-6 mx-16">
         <h1 className="font-semibold text-3xl text-light-red">Unesco Intangible</h1>
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-6 mx-16">
         {DataUNESCOIntangible.map((item) => (
           <UNESCOIntangibleCard
             key={item.id}
@@ -26,8 +26,8 @@ const UNESCORecognition = () => {
           />
         ))}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-6 mx-16">
         <h1 className="font-semibold text-3xl text-light-red">Unesco Heritage</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-7 mt-6 mx-16">
         {DataUNESCOHeritage.map((item) => (
           <CardUnescoHeritage
             key={item.id}
