095ecb4160334feda481b3d7c3114f88f42d9e93
ayuarista <aristaadewi8@gmail.com>
2025-03-02 20:01:54 +0800

    add UNESCO intangible cultural heritage data entries

---
+++ src/data/card/UNESCOIntangible.jsx
@@ -0,0 +1,44 @@
+export const UNESCOIntangible = [
+    {
+        id: 1,
+        image: "https://images.unsplash.com/photo-1662793524504-bd11271b4b56?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2F5YW5nfGVufDB8fDB8fHww",
+        title: "Wayang",
+        desc: "Traditional Indonesian Wayang Shadow and Wayang Golek performing arts.",
+        year: "2003",
+    },
+    {
+        id: 2,
+        image: "https://cdn.rri.co.id/berita/Singaraja/o/1731380885587-WhatsApp_Image_2024-11-12_at_11.04.41_(2)/zyb5rsz7zz9x7i4.jpeg",
+        title: "Keris",
+        desc: "Traditional weapons that have high philosophical value typical of Indonesia.",
+        year: "2005",
+    },
+    {
+        id: 3,
+        image: "",
+        title: "Batik",
+        desc: "The art technique of coloring Indonesian batik cloth.",
+        year: "2009",
+    },
+    {
+        id: 4,
+        image: "https://akcdn.detik.net.id/community/media/visual/2023/05/07/puspa-raih-emas-nomor-seni-tunggal-putri-pencak-silat_169.jpeg?w=700&q=90",
+        title: "Pencak Silat",
+        desc: "Pencak Silat is a traditional martial art that is very typical of Indonesia.",
+        year: "2019",
+    },
+    {
+        id: 5,
+        image: "https://indonesiabuzz.com/wp-content/uploads/2024/01/Desain-tanpa-judul-81.jpg",
+        title: "Gamelan",
+        desc: "A traditional Indonesian music ensemble originating from Java and Bali.",
+        year: "2021",
+    },
+    {
+        id: 6,
+        image: "https://t4.ftcdn.net/jpg/02/36/54/03/360_F_236540366_HiL7OB78o3cKuI2mnbHmmJ439kpfbe4j.jpg",
+        title: "Jamu",
+        desc: "Traditional medicine based on Indonesian herbal concoctions.",
+        year: "2023",
+    },
+]
