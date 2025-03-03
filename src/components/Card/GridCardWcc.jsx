f2bdc8f08572fed8b273f32048a5d82b078c4f57
ayuarista <aristaadewi8@gmail.com>
2025-03-03 21:11:27 +0800

    enhance GridCardWcc component with additional images and scrolling animation

--- src/components/Card/GridCardWcc.jsx
+++ src/components/Card/GridCardWcc.jsx
@@ -1,13 +1,42 @@
-const GridCardWcc= () => {
-    return(
-        <div className="flex items-center justify-center gap-3">
-            <div>
-                <img src="https://images.unsplash.com/photo-1672716912467-fd99b71cf780?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D" alt="" className="w-80 object-cover rounded-lg"/>
-                <img src="https://asset.kompas.com/crops/tM87uX0fIermRYOG0NtO0RSpabE=/64x43:975x650/1200x800/data/photo/2023/04/25/644781af44215.jpg" alt="" className="w-80 object-cover rounded-lg"/>
-                <img src="https://images.unsplash.com/photo-1609407683391-7d127a00b3e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D" alt="" className="w-80 object-cover rounded-lg"/>
-            </div>
-        </div>
-    )
-}
+
+
+const GridCardWcc = () => {
+  return (
+    <div className="overflow-hidden w-full">
+      <div className="flex gap-3 w-[200%] slider">
+        <img
+          src="https://images.unsplash.com/photo-1672716912467-fd99b71cf780?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"
+          alt=""
+          className="w-80 object-cover rounded-lg"
+        />
+        <img
+          src="https://asset.kompas.com/crops/tM87uX0fIermRYOG0NtO0RSpabE=/64x43:975x650/1200x800/data/photo/2023/04/25/644781af44215.jpg"
+          alt=""
+          className="w-80 object-cover rounded-lg"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1609407683391-7d127a00b3e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D"
+          alt=""
+          className="w-80 object-cover rounded-lg"
+        />
+                <img
+          src="https://images.unsplash.com/photo-1672716912467-fd99b71cf780?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"
+          alt=""
+          className="w-80 object-cover rounded-lg"
+        />
+        <img
+          src="https://asset.kompas.com/crops/tM87uX0fIermRYOG0NtO0RSpabE=/64x43:975x650/1200x800/data/photo/2023/04/25/644781af44215.jpg"
+          alt=""
+          className="w-80 object-cover rounded-lg"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1609407683391-7d127a00b3e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D"
+          alt=""
+          className="w-80 object-cover rounded-lg"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default GridCardWcc;
--- src/components/Molecules/WorldCraftsCouncil.jsx
+++ src/components/Molecules/WorldCraftsCouncil.jsx
@@ -1,16 +1,19 @@
 import HeaderInternationalAwards from "../Atoms/HeaderInternationalAwards";
-import wcc from "/assets/awards/wcc.svg"
+import wcc from "/assets/awards/wcc.svg";
 import AboutASEAN from "../Atoms/AboutASEAN";
 import CardWCC from "../Card/CardWCC";
+import GridCardWcc from "../Card/GridCardWcc";
 const WorldCraftsCouncil = () => {
-    return(
-        <>
-        <HeaderInternationalAwards
+  return (
+    <>
+      <HeaderInternationalAwards
         image={wcc}
         title={"World Crafts Council (WCC)"}
-        desc={"The World Crafts Council (WCC) is an international organization focused on the development and preservation of traditional crafts worldwide. Indonesia has received several recognitions from the WCC for its contribution to preserving the art and culture of handicrafts."}
-        />
-              <div className="flex items-center justify-center">
+        desc={
+          "The World Crafts Council (WCC) is an international organization focused on the development and preservation of traditional crafts worldwide. Indonesia has received several recognitions from the WCC for its contribution to preserving the art and culture of handicrafts."
+        }
+      />
+      <div className="flex items-center justify-center">
         <div className="w-full">
           <AboutASEAN
             title={'Pekalongan as "World City of Batik" (2014)'}
@@ -20,11 +23,12 @@ const WorldCraftsCouncil = () => {
           />
         </div>
         <div className="w-1/2">
-        <CardWCC />
+          <CardWCC />
         </div>
       </div>
-        </>
-    )
-}
+      <GridCardWcc/>
+    </>
+  );
+};
 
 export default WorldCraftsCouncil;
