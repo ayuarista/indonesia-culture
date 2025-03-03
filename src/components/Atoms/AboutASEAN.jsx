231053e53ebd5460bc218a10acbe737837cc7c97
ayuarista <aristaadewi8@gmail.com>
2025-03-03 13:38:01 +0800

    add AboutASEAN component for displaying title and description

---
+++ src/components/Atoms/AboutASEAN.jsx
@@ -0,0 +1,10 @@
+const AboutASEAN = ({title, desc}) => {
+    return(
+        <div>
+            <h1 className="font-bold text-light-red text-3xl">{title}</h1>
+            <p className="font-medium text-sm mt-1">{desc}</p>
+        </div>
+    )
+}
+
+export default AboutASEAN
