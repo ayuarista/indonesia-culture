3c32db7e23d76aabeff310cca7d83262eb46f40e
ayuarista <aristaadewi8@gmail.com>
2025-03-03 14:05:07 +0800

    add CardAseanCultural component for displaying cultural images

---
+++ src/components/Card/CardAseanCultural.jsx
@@ -0,0 +1,16 @@
+const CardAseanCultural = () => {
+    return(
+        <div className="relative bg-gradient-to-r from-[#430304] to-[#A90809] w-[80%] h-72 rounded-tr-full rounded-br-full">
+            <div className="absolute top-2 left-16 lg:left-24">
+                <img src="https://images.unsplash.com/photo-1670179716487-5acdf98c9646?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTR8fGluZG9uZXNpYW4lMjBjdWx0dXJlfGVufDB8fDB8fHww" alt="" className="w-56 h-28 object-cover rounded-tl-4xl rounded-tr-4xl rounded-bl-4xl border-[5px] border-white" />
+            </div>
+            <div className="absolute top-5 -right-20 lg:-right-8">
+                <img src="https://images.unsplash.com/photo-1620409123739-3849e65df57a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2F5YW5nJTIwa3VsaXR8ZW58MHx8MHx8fDA%3D" alt="" className="w-52 h-48 object-cover rounded-tl-4xl rounded-tr-4xl rounded-br-4xl border-[5px] border-white" />
+                <div className="absolute -bottom-[60px] -left-16 lg:-left-4">
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardAseanCultural;
