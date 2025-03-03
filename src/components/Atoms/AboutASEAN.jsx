81e5530b54ad0671bcb38c5a8deeeb4a3cfb05fe
ayuarista <aristaadewi8@gmail.com>
2025-03-03 14:02:26 +0800

    update AboutASEAN component styles and integrate into ASEAN component

--- src/components/Atoms/AboutASEAN.jsx
+++ src/components/Atoms/AboutASEAN.jsx
@@ -2,7 +2,7 @@ const AboutASEAN = ({title, desc}) => {
     return(
         <div>
             <h1 className="font-bold text-light-red text-3xl">{title}</h1>
-            <p className="font-medium text-sm mt-1">{desc}</p>
+            <p className="font-medium text-sm mt-1 text-black max-w-[30rem] text-pretty">{desc}</p>
         </div>
     )
 }
--- src/components/Molecules/ASEAN.jsx
+++ src/components/Molecules/ASEAN.jsx
@@ -1,3 +1,4 @@
+import AboutASEAN from "../Atoms/AboutASEAN";
 import HeaderInternationalAwards from "../Atoms/HeaderInternationalAwards";
 import ImageSlider from "./ImageSlider";
 import asean from "/assets/awards/asean.svg"
@@ -8,6 +9,10 @@ const ASEAN = () => {
         image={asean}
         title={"ASEAN Cultural Awards"}
         desc={"Awards from ASEAN (Association of Southeast Asian Nations) are a form of appreciation given by ASEAN to individuals, organizations or member countries that have made extraordinary contributions in various fields, one of which is Culture."}
+        />
+        <AboutASEAN
+        title={"ASEAN Cultural Heritage Award"}
+        desc={"The ASEAN Cultural Heritage Award honors significant contributions to preserving, protecting, and promoting the rich and diverse cultural heritage of Southeast Asia."}
         />
         <ImageSlider/>
         </>
