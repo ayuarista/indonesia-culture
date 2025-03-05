458099959560529a68c655cfc0f4d1411864b7e9
ayuarista <aristaadewi8@gmail.com>
2025-03-05 22:34:26 +0800

    style: refactor CardPreserveCulture component to use destructured props for improved readability

--- src/components/Card/CardPreserveCulture.jsx
+++ src/components/Card/CardPreserveCulture.jsx
@@ -1,19 +1,19 @@
-const CardPreserveCulture = (props) => {
+const CardPreserveCulture = ({image, title, desc}) => {
     return (
         <div className="relative max-w-md lg:max-w-sm border border-gray-300 rounded-lg p-5">
             <div className="relative">
-            <img src={props.image} 
-            alt={props.title}
+            <img src={image} 
+            alt={title}
             className="w-full h-52 object-center object-cover rounded-tl-xl rounded-br-xl"
             />
             <div className="absolute bottom-0 left-0 right-0 rounded-br-xl h-28 hover:h-52  transition-all duration-300 ease-in-out  bg-gradient-to-t from-light-red via-black/40 to-transparent px-5 flex items-end">
             <h1 className="absolute bottom-2 font-bold text-[18px] text-white">
-                {props.title}
+                {title}
             </h1>
             </div>
             </div>
             <div className="p-2">
-                <p className="text-justify font-medium text-[14px] text-black mt-2">{props.desc}</p>
+                <p className="text-justify font-medium text-[14px] text-black mt-2">{desc}</p>
             </div>
         </div>
     )
