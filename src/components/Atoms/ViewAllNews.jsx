e346a714f67622ad2ffc84d0860af02d21f7dd31
ayuarista <aristaadewi8@gmail.com>
2025-03-05 22:22:39 +0800

    style: refactor ViewAllNews component to improve button interactivity and structure

--- src/components/Atoms/ViewAllNews.jsx
+++ src/components/Atoms/ViewAllNews.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 const ViewAllNews = () => {
-    return(
-        <button className="px-5 py-2 rounded-full border  border-[#B20607] hover:bg-light-red hover:text-white text-[#B20607] ">
-        <Link to="/article">
-            <p className="font-semibold text-[14px]">View All News</p>
-        </Link>
-        </button>
-    )
-}
+  return (
+    <Link to="/article">
+      <button className="px-5 py-2 rounded-full border border-[#B20607] transition-all duration-300 hover:bg-light-red hover:text-white text-[#B20607] hover:cursor-pointer">
+        <p className="font-semibold text-[14px]">View All News</p>
+      </button>
+    </Link>
+  );
+};
 
 export default ViewAllNews;
