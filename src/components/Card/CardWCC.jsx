2f4aa68ffc45bc4eadb6db5e109c69372c75554e
ayuarista <aristaadewi8@gmail.com>
2025-03-06 22:26:13 +0800

    style: update border width in CardWCC component for improved visual consistency

--- src/components/Card/CardWCC.jsx
+++ src/components/Card/CardWCC.jsx
@@ -5,7 +5,7 @@ const CardWCC = () => {
                 <img src="https://asset-2.tstatic.net/jateng/foto/bank/images/Peresmian-gedung-baru-perpustakaan-daerah-Kota-Pekalongan.jpg" alt="" className="w-48 h-56 object-cover rounded-full border-[8px] border-light-red  -rotate-6 shadow-2xl" />
             </div>
             <div className="absolute bottom-3 right-10">
-                <img src="https://kotomono.co/wp-content/uploads/2016/12/Museum-Batik-Pekalongan-2020-750x536.jpg" alt="" className="w-48 h-52 rotate-6 object-cover rounded-tl-4xl rounded-b-4xl border-[7px] border-white shadow-xl" />
+                <img src="https://kotomono.co/wp-content/uploads/2016/12/Museum-Batik-Pekalongan-2020-750x536.jpg" alt="" className="w-48 h-52 rotate-6 object-cover rounded-tl-4xl rounded-b-4xl border-8 border-white shadow-xl" />
             <div className="absolute -bottom-2 right-0 p-6 bg-light-red rounded-full border-4 border-white"></div>
             </div>
         </div>
