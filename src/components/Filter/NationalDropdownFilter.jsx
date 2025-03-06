d117967c1a789f9f29ef8606e5f6d218f2043108
ayuarista <aristaadewi8@gmail.com>
2025-03-06 21:57:29 +0800

    style: center align items in NationalDropdownFilter for improved layout

--- src/components/Filter/NationalDropdownFilter.jsx
+++ src/components/Filter/NationalDropdownFilter.jsx
@@ -80,7 +80,7 @@ const NationalDropdownFilter = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-6 mx-10 lg:mx-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-6 mx-10 lg:mx-16 place-items-center">
         {filteredData.map((item) => (
           <CardNational
             key={item.id}
