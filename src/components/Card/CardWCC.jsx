f1d328cd4bc03a5204dec9e44825f5153ed0d1a3
ayuarista <aristaadewi8@gmail.com>
2025-03-08 22:52:28 +0800

    style: adjust positioning of images in CardWCC component for improved layout

--- src/components/Card/CardWCC.jsx
+++ src/components/Card/CardWCC.jsx
@@ -1,10 +1,10 @@
 const CardWCC = () => {
     return(
         <div className="relative mt-10 bg-gradient-to-r from-0% to-80% from-[#430304] to-[#A90809] w-full h-36 rounded-tl-full rounded-bl-full">
-            <div className="absolute -top-10 left-0 z-10">
+            <div className="absolute -top-10 left-4 lg:left-0 z-10">
                 <img src="https://asset-2.tstatic.net/jateng/foto/bank/images/Peresmian-gedung-baru-perpustakaan-daerah-Kota-Pekalongan.jpg" alt="" className="w-48 h-56 object-cover rounded-full border-[8px] border-light-red  -rotate-6 shadow-2xl" />
             </div>
-            <div className="absolute bottom-3 right-10">
+            <div className="absolute bottom-3 right-5 lg:right-10">
                 <img src="https://kotomono.co/wp-content/uploads/2016/12/Museum-Batik-Pekalongan-2020-750x536.jpg" alt="" className="w-48 h-52 rotate-6 object-cover rounded-tl-4xl rounded-b-4xl border-8 border-white shadow-xl" />
             <div className="absolute -bottom-2 right-0 p-6 bg-light-red rounded-full border-4 border-white"></div>
             </div>
