1d8a1c6bfc03493aa7d25179a165d3300a822165
ayuarista <aristaadewi8@gmail.com>
2025-03-08 22:53:43 +0800

    style: adjust font size and margin in AboutWcc component for improved readability

--- src/components/Atoms/AboutWcc.jsx
+++ src/components/Atoms/AboutWcc.jsx
@@ -1,8 +1,8 @@
 const AboutWcc = () => {
     return(
         <div className="flex flex-col items-center justify-center text-center mt-10">
-            <h1 className="text-light-red font-bold text-3xl">Recognition of Sumba Ikat Weaving as a <br/> World Cultural Heritage (2017)</h1>
-            <p className="max-w-[40rem] text-pretty font-medium text-[15px] text-black mt-2">WCC recognizes Sumbanese Ikat Weaving as a world cultural heritage because of its unique motifs and traditional techniques. This award encourages the preservation and empowerment of weaving artisans in Sumba.</p>
+            <h1 className="text-light-red font-bold text-2xl lg:text-3xl">Recognition of Sumba Ikat Weaving as a <br/> World Cultural Heritage (2017)</h1>
+            <p className="max-w-[40rem] text-pretty font-medium text-sm lg:text-[15px] text-black mt-3">WCC recognizes Sumbanese Ikat Weaving as a world cultural heritage because of its unique motifs and traditional techniques. This award encourages the preservation and empowerment of weaving artisans in Sumba.</p>
         </div>
     )
 
