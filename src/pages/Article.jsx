8ad6e424912501e3b852d80308883c3945e10869
ayuarista <aristaadewi8@gmail.com>
2025-03-10 22:51:40 +0800

    feat: render CardArticle components dynamically from DataCardArticle in Article page

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -3,7 +3,17 @@ import { DataCardArticle } from "../data/card/DataCardArticle";
 const Article = () => {
     return(
         <div className="pt-16">
-            
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
+                {DataCardArticle.map((article) => (
+                    <CardArticle
+                        key={article.id}
+                        image={article.image}
+                        title={article.title}
+                        author={article.author}
+                        link={article.link}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
