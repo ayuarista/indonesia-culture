20a2c470c8a4d43f71879c8a343397cf142da8ea
ayuarista <aristaadewi8@gmail.com>
2025-03-10 22:47:45 +0800

    feat: add DataCardArticle data structure with sample articles and metadata

---
+++ src/data/card/DataCardArticle.jsx
@@ -0,0 +1,100 @@
+export const DataCardArticle = [
+    {
+        id: 1,
+        image: "https://images.unsplash.com/photo-1666587128445-4623f8f8f033?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8amF2YSUyMGRhbmNlfGVufDB8fDB8fHww",
+        author: "Cici Dian Eviliya",
+        title: "Often Thought to be Showing Off Their Bodies, It Turns Out This is the Dancers' Effort to Preserve Culture",
+        link: "https://www.kompasiana.com/cicidian5506/677d5ab134777c0b59547556/sering-dikira-pamer-tubuh-ternyata-ini-usaha-penari-untuk-melestarikan-budaya"
+    },
+    {
+        id: 2,
+        image: "https://cdn.antaranews.com/cache/1200x800/2024/12/05/1000059187.jpg.webp",
+        author: "Sonya Hellen Sinombor",
+        title: "Kolintang Recognized by UNESCO as Intangible Cultural Heritage, Great Responsibility Awaits",
+        link: "https://www.kompas.id/artikel/kolintan-warisan-budaya-unesco"
+    },
+    {
+        id: 3,
+        image: "https://images.unsplash.com/photo-1727838604328-b31d7eef9a57?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fEluZG9uZXNpYSUyMGN1bHR1cmV8ZW58MHx8MHx8fDA%3D",
+        author: "Indah Novita Dewi",
+        title: "Preserving Traditional Dance Through Wedding Reception Events",
+        link: "https://www.kompasiana.com/indahnovita5600/6677d6b934777c6147595a34/melestarikan-tari-tradisional-melalui-acara-resepsi-pernikahan"
+    },
+    // {
+    //     id: 4,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 5,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 6,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 7,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 8,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 9,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 10,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 11,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 12,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 13,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+    // {
+    //     id: 14,
+    //     image: "",
+    //     author: "",
+    //     title: "",
+    //     link: ""
+    // },
+]
