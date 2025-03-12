181bd585b8987296e9221e6894727a92182998d2
ayuarista <aristaadewi8@gmail.com>
2025-03-12 22:58:55 +0800

    fix: update positioning of elements in CardWCC component for improved layout

--- src/components/Card/CardWCC.jsx
+++ src/components/Card/CardWCC.jsx
@@ -4,7 +4,7 @@ const CardWCC = () => {
             <div className="absolute -top-10 left-4 lg:left-0 z-10">
                 <img src="https://asset-2.tstatic.net/jateng/foto/bank/images/Peresmian-gedung-baru-perpustakaan-daerah-Kota-Pekalongan.jpg" alt="" className="w-48 h-56 object-cover rounded-full border-[8px] border-light-red  -rotate-6 shadow-2xl" />
             </div>
-            <div className=" bottom-3 right-5 lg:right-10">
+            <div className="absolute bottom-3 right-5 lg:right-10">
                 <img src="https://kotomono.co/wp-content/uploads/2016/12/Museum-Batik-Pekalongan-2020-750x536.jpg" alt="" className="w-48 h-52 rotate-6 object-cover rounded-tl-4xl rounded-b-4xl border-8 border-white shadow-xl" />
             <div className="absolute -bottom-2 right-0 p-6 bg-light-red rounded-full border-4 border-white"></div>
             </div>
