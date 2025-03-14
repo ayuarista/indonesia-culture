d8835f3717a32295897de477f3606c945268ada7
ayuarista <aristaadewi8@gmail.com>
2025-03-14 21:39:33 +0800

    nhj

--- src/components/Card/ArticleCard.jsx
+++ src/components/Card/ArticleCard.jsx
@@ -5,7 +5,7 @@ const ArticleCard = ({ image, label, author, date, title, size, text, sizeTitle 
         <div className="absolute text-sm -left-2 rounded-br-full rounded-tr-full shadow-[0px_4px_4px_3px_rgba(0,0,0,0.25)] px-4 bg-white text-[#B20607] font-semibold p-1.5">
           {label}
         </div>
-        <div className="absolute bottom-0 left-0 right-0 h-36 bg-gradient-to-t rounded-lg from-black/90 to-transparent p-4 flex flex-col justify-end">
+        <div className=" bottom-0 left-0 right-0 h-36 bg-gradient-to-t rounded-lg from-black/90 to-transparent p-4 flex flex-col justify-end">
           <div className="border-l-4 border-[#AF0808] px-3">
             <p className={`text-white ${text}`}>{author} | {date}</p>
             <h1 className={`text-white font-bold ${sizeTitle}`}>{title}</h1>
