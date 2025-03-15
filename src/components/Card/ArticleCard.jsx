ec721d8d12c5c7b288d336fccac1f1b6d727ed35
ayuarista <aristaadewi8@gmail.com>
2025-03-15 23:42:25 +0800

    fix: remove unnecessary class from ArticleCard component for cleaner markup

--- src/components/Card/ArticleCard.jsx
+++ src/components/Card/ArticleCard.jsx
@@ -2,7 +2,7 @@ const ArticleCard = ({ image, date, title, size, text, sizeTitle }) => {
     return (
       <div className={` ${size}`}>
         <img src={image} alt="News" className="w-full h-full object-cover object-center rounded-lg" />
-        <div className="absolute text-sm -left-2 rounded-br-full rounded-tr-full top-4 shadow-[0px_4px_4px_3px_rgba(0,0,0,0.25)] px-4 bg-white text-[#B20607] font-semibold p-1.5">
+        <div className=" text-sm -left-2 rounded-br-full rounded-tr-full top-4 shadow-[0px_4px_4px_3px_rgba(0,0,0,0.25)] px-4 bg-white text-[#B20607] font-semibold p-1.5">
           {label}
         </div>
         <div className="absolute bottom-0 left-0 right-0 h-36 bg-gradient-to-t rounded-lg from-black/90 to-transparent p-4 flex flex-col justify-end">
