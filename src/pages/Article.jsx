22a3bad950478a5675c381fc6e6dbde6cd65dcbf
ayuarista <aristaadewi8@gmail.com>
2025-03-18 22:27:19 +0800

    fix: add padding to grid layout in Article component for improved spacing

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -3,7 +3,7 @@ import { DataCardArticle } from "../data/card/DataCardArticle";
 const Article = () => {
     return(
         <div className="pt-16">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
                 {DataCardArticle.map((article) => (
                     <CardArticle
                         key={article.id}
