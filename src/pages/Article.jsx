601288c81558ff52fe72f6c743db66a71553ba91
ayuarista <aristaadewi8@gmail.com>
2025-03-25 23:26:36 +0800

    fix: update link prop to use article.link in Article component

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -10,7 +10,7 @@ const Article = () => {
                         image={article.image}
                         title={article.title}
                         author={article.author}
-                        link={item.link}
+                        link={article.link}
                     />
                 ))}
             </div>
