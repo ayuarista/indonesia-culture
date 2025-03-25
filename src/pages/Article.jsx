c5b3af798e771d912919e76479f17b55f38280cc
ayuarista <aristaadewi8@gmail.com>
2025-03-25 23:30:52 +0800

    fix: correct grid layout className in Article component

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -3,7 +3,7 @@ import { DataCardArticle } from "../data/card/DataCardArticle";
 const Article = () => {
     return(
         <div className="pt-16">
-            <div className=" -cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
+            <div className=" grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
                 {DataCardArticle.map((article) => (
                     <CardArticle
                         key={article.id}
