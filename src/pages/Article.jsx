fe816b5bb65e9d50955665abdede145dcb2bd13d
ayuarista <aristaadewi8@gmail.com>
2025-03-26 23:11:23 +0800

    fix: remove unused import of DataCardArticle in Article component

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,5 +1,4 @@
 import CardArticle from "../components/Card/CardArticle";
-import { DataCardArticle } from "../data/card/DataCardArticle";
 const Article = () => {
     return(
         <div className="pt-16">
