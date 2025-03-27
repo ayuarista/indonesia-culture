b85803e0f22f63b25e63502331c6ac85f80bab1d
ayuarista <aristaadewi8@gmail.com>
2025-03-27 22:30:36 +0800

    fix: update positioning class for image container in CardTVRI component

--- src/components/Card/CardTVRI.jsx
+++ src/components/Card/CardTVRI.jsx
@@ -11,7 +11,7 @@ const CardTVRI = () => {
         <p className="font-medium text-black text-pretty mt-3 max-w-[25rem] lg:max-w-[85%] text-justify text-sm">RRI, established on September 11, 1945, is Indonesia's national radio station that provides news, culture, and education. As a member of ABU, RRI plays a role in international content exchange and development of broadcasting technology. </p>
       </div>
       <div className="relative w-full bg-gradient-to-r from-dark-red to-light-red h-72 rounded-tl-full">
-        <div className=" right-8 lg:right-12 -top-10">
+        <div className="absolute right-8 lg:right-12 -top-10">
           <img
             src="https://cdn.antaranews.com/cache/1200x800/2023/11/23/WhatsApp-Image-2023-11-23-at-14.44.23.jpeg.webp"
             className="rounded-tr-full rounded-tl-full object-cover w-60 h-60 lg:w-72 lg:h-72 border-8 border-white rotate-6"
