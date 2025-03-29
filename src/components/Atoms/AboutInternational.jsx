01bbb408a1faadcfde0a5c016ea93e179d0585f3
ayuarista <aristaadewi8@gmail.com>
2025-03-29 16:14:03 +0800

    fix: adjust top margin in AboutInternational component

--- src/components/Atoms/AboutInternational.jsx
+++ src/components/Atoms/AboutInternational.jsx
@@ -1,6 +1,6 @@
 const AboutInternational = () => {
     return(
-        <div className="mt-10 lg:mt-0">
+        <div className="mt-4 lg:mt-0">
             <h1 className="font-bold text-3xl lg:text-4xl bg-gradient-to-r text-transparent bg-clip-text from-0% to-20% from-dark-red to-light-red">Indonesian International <br /> Cultural Awards</h1>
             <p className="text-pretty text-justify font-medium mt-2 text-black text-[15px] lg:text-base">Indonesia's international cultural awards highlight its rich ancestral heritage. Recognition from UNESCO, Guinness World Records, ASEAN, and the World Crafts Council affirms its universal value and the need for preservation.</p>
         </div>
