6c8adf5742a5aaa884ed8900bb2c3b1a85f01e16
ayuarista <aristaadewi8@gmail.com>
2025-04-04 12:04:53 +0800

    fix: enhance responsiveness in CardProtectCulture and ProtectCulture components for medium screens

--- src/components/Card/CardProtectCulture.jsx
+++ src/components/Card/CardProtectCulture.jsx
@@ -1,11 +1,11 @@
 const CardProtectCulture = ({image, title, desc, place}) => {
   return (
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col justify-center items-center">
         <div className="relative overflow-hidden rounded-t-full border-white border-[8px]">
           <img
             src={image}
             alt=""
-            className="w-68 h-64 lg:w-80 lg:h-80 object-cover"
+            className="w-68 h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 object-cover"
           />
           <div className="absolute bottom-0 left-0 right-0 h-80 hover:h-96 transition-all duration-300 bg-gradient-to-t from-black to-transparent p-5 flex items-end">
             <div className="flex flex-col">
