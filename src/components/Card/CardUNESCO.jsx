08e5fb00e00523fea6741c331c23c2ab9eb80339
ayuarista <aristaadewi8@gmail.com>
2025-04-04 11:48:02 +0800

    fix: update layout in CardUNESCO component for improved responsiveness on medium screens

--- src/components/Card/CardUNESCO.jsx
+++ src/components/Card/CardUNESCO.jsx
@@ -1,28 +1,28 @@
 const CardUNESCO = () => {
     return (
-        <div className="relative bg-gradient-to-r from-[#430304] to-[#A90809] w-[90%] lg:w-[80%] h-72 rounded-tr-full rounded-br-full">
-            <div className="absolute top-2 left-6 lg:left-24">
+        <div className="relative bg-gradient-to-r from-[#430304] to-[#A90809] w-[90%] md:w-[70%] lg:w-[80%] h-72 rounded-tr-full rounded-br-full">
+            <div className="absolute top-2 left-6 md:left-16 lg:left-24">
                 <img src="https://images.unsplash.com/photo-1733049919526-3ccf0ae8f6ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGNhbmRpJTIwcHJhbWJhbmFufGVufDB8fDB8fHww"
                  alt="" 
-                 className="w-48 lg:w-56 h-28 object-cover rounded-tl-4xl rounded-tr-4xl rounded-bl-4xl border-[5px] border-white"
+                 className="w-48 md:w-52 lg:w-56 h-28 object-cover rounded-tl-4xl rounded-tr-4xl rounded-bl-4xl border-[5px] border-white"
                  />
             </div>
-            <div className="absolute -top-0 lg:top-5 -right-10 lg:-right-8">
+            <div className="absolute -top-0 lg:top-5  md:top-4 -right-10 md:right-20 lg:-right-8">
                 <img src="https://images.unsplash.com/photo-1620409123739-3849e65df57a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2F5YW5nJTIwa3VsaXR8ZW58MHx8MHx8fDA%3D"
                  alt="" 
-                 className="w-48 lg:w-52 h-48 object-cover rounded-tl-4xl rounded-tr-4xl rounded-br-4xl border-[5px] border-white"
+                 className="w-48 md:w-52 lg:w-52 h-48 object-cover rounded-tl-4xl rounded-tr-4xl rounded-br-4xl border-[5px] border-white"
                  />
             </div>
-            <div className="absolute -bottom-[68px] left-6 lg:left-24">
+            <div className="absolute -bottom-[68px] left-6 md:left-16 lg:left-24">
                 <img src="https://images.unsplash.com/photo-1586319826907-1ff4aadbaddc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmF0aWt8ZW58MHx8MHx8fDA%3D"
                  alt="" 
-                 className="w-48 lg:w-56 object-left h-56 object-cover rounded-tl-4xl rounded-br-4xl rounded-bl-4xl border-[5px] border-white"
+                 className="w-48 md:w-52 lg:w-56 object-left h-56 object-cover rounded-tl-4xl rounded-br-4xl rounded-bl-4xl border-[5px] border-white"
                  />
             </div>
-            <div className="absolute -bottom-[42px] lg:-bottom-[60px] -right-9 lg:-right-4">
+            <div className="absolute -bottom-[42px] md:-bottom-20 lg:-bottom-[60px] -right-9 md:right-[75px] lg:-right-4">
                 <img src="https://images.unsplash.com/photo-1556375413-f6cdc5e17398?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGJvcm9idWR1cnxlbnwwfHwwfHx8MA%3D%3D"
                  alt="" 
-                 className="w-full h-32 object-cover rounded-tr-4xl rounded-br-4xl rounded-bl-4xl border-[5px] border-white"
+                 className="w-full h-32 md:h-36 object-cover rounded-tr-4xl rounded-br-4xl rounded-bl-4xl border-[5px] border-white"
                  />
             </div>
         </div>
