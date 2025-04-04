0d9b90c335384b659030eb0adc79f400f8fb3631
ayuarista <aristaadewi8@gmail.com>
2025-04-04 11:37:29 +0800

    fix: adjust CardRekorMuri layout for improved responsiveness on larger screens

--- src/components/Card/CardRekorMuri.jsx
+++ src/components/Card/CardRekorMuri.jsx
@@ -2,8 +2,8 @@ const CardRekorMuri = () => {
   return (
     <>
       <div className="relative mt-12 md:mt-0 lg:mt-0">
-        <div className="absolute right-0 bottom-2 p-8 w-[70%] md:w-1/2 bg-linear-to-r from-20% from-[#A90809] to-50% to-[#430304] rounded-tl-full rounded-bl-full"></div>
-        <div className="absolute right-0 top-2 p-10 w-[80%] md:w-[60%] bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full">
+        <div className="absolute right-0 bottom-2 p-8 w-[70%] md:w-1/2 lg:w-[70%] bg-linear-to-r from-20% from-[#A90809] to-50% to-[#430304] rounded-tl-full rounded-bl-full"></div>
+        <div className="absolute right-0 top-2 p-10 w-[80%] md:w-[60%] lg:w-[80%] bg-gradient-to-r from-[#A90809] to-[#430304] rounded-tl-full rounded-bl-full">
         <div className="absolute w-32 md:w-40 lg:w-36 -bottom-9 md:-bottom-5 md:left-32 left-8 lg:left-20">
           <img
             src="https://images.unsplash.com/photo-1670179716382-42dfa4f0d5b4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGluZG9uZXNpYW4lMjBjdWx0dXJlfGVufDB8fDB8fHww"
