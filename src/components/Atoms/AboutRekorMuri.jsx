112dc7e06346784cba33371ff984026789d18fc5
ayuarista <aristaadewi8@gmail.com>
2025-04-05 22:13:53 +0800

    fix: enhance layout and responsiveness in AboutRekorMuri component for improved display on medium and large screens

--- src/components/Atoms/AboutRekorMuri.jsx
+++ src/components/Atoms/AboutRekorMuri.jsx
@@ -1,21 +1,27 @@
-import ViewAllNews from "./ViewAllNews"
-import muri from "/assets/home/muri.svg"
+import ViewAllNews from "./ViewAllNews";
+import muri from "/assets/home/muri.svg";
 const AboutRekorMuri = () => {
-    return (
-        <div className="mx-8 lg:mx-16 mt-40 lg:mt-0">
-            <div className="flex items-center gap-3">
-                <div>
-                <h1 className="bg-gradient-to-r font-bold text-3xl from-0% from-[#490303] to-60% to-[#AF0808] text-transparent bg-clip-text">Indonesian Muri Record</h1>
-                </div>
-                <img src={muri} alt="" className="w-36 lg:w-28"/>
-            </div>
-            <div className="mt-3 text-pretty">
-            <p className="font-medium text-justify md:max-w-[70%] text-black">The Indonesia World Records Museum is a museum located in Semarang, Central Java, Indonesia. A collection of Indonesian records is presented in this museum.</p>
-            </div>
-            <div className="mt-5">
-                <ViewAllNews/>
-            </div>
+  return (
+    <div className="mx-8 lg:mx-16 mt-40 lg:mt-0">
+      <div className="flex items-center gap-3">
+        <div>
+          <h1 className="bg-gradient-to-r font-bold text-3xl from-0% from-[#490303] to-60% to-[#AF0808] text-transparent bg-clip-text">
+            Indonesian Muri Record
+          </h1>
         </div>
-    )
-}
-export default AboutRekorMuri
+        <img src={muri} alt="" className="w-36 lg:w-28" />
+      </div>
+      <div className="mt-3 text-pretty">
+        <p className="font-medium text-justify md:max-w-[70%] lg:max-w-full text-black">
+          The Indonesia World Records Museum is a museum located in Semarang,
+          Central Java, Indonesia. A collection of Indonesian records is
+          presented in this museum.
+        </p>
+      </div>
+      <div className="mt-5">
+        <ViewAllNews />
+      </div>
+    </div>
+  );
+};
+export default AboutRekorMuri;
