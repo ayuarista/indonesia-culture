962fd60319ba83bb5cd3cfcac4ec6049530c6d52
ayuarista <aristaadewi8@gmail.com>
2025-04-07 23:05:01 +0800

    feat: add CardCalenderEvents component and integrate it into Province page

---
+++ src/components/Card/CardCalenderEvents.jsx
@@ -0,0 +1,42 @@
+import { FaLocationDot } from "react-icons/fa6";
+import { MdOutlineDateRange } from "react-icons/md";
+function CardCalenderEvents() {
+  return (
+    <div className="relative rounded-2xl shadow-lg bg-white">
+      <div className="relative">
+        <img
+          src="https://images.unsplash.com/photo-1609826704020-26abbd0879c3?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          className="w-full h-54 object-cover rounded-2xl"
+        />
+        <div className="absolute rounded-full px-2 py-2 bg-gradient-to-l from-dark-red to-light-red text-white font-semibold top-3 mx-2">
+          <p className="text-[13px]">Remaining Days : 470 Days</p>
+        </div>
+      </div>
+      <div className="absolute rounded-2xl bg-white h-60 top-28 bottom-0 shadow-lg">
+        <div className="p-3">
+        <div className="flex items-center gap-3">
+            <div className="flex items-center gap-2 bg-red-100 px-3 rounded-full py-1">
+              <FaLocationDot className="text-light-red" />
+              <p className="text-light-red text-sm">Bali</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <MdOutlineDateRange className="text-light-red" />
+              <p className="text-gray-400 text-sm">Maret 2024</p>
+            </div>
+          </div>
+          <div className="mx-2 mt-1">
+          <h1 className="font-semibold text-black text-lg ">Hari Raya Nyepi</h1>
+          <p className="text-sm text-justify text-gray-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi ut
+            et assumenda corrupti quibusdam nam natus. Veritatis, tenetur
+            necessitatibus sed quas explicabo soluta impedit alias ut?
+            Repellendus recusandae corrupti necessitatibus?
+          </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default CardCalenderEvents;
--- src/pages/Province.jsx
+++ src/pages/Province.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import CalenderEvents from "./CalenderEvents";
+import CardCalenderEvents from "../components/Card/CardCalenderEvents";
 
 const Province = () => {
   return (
-    <div className="pt-28">
+    <div className="pt-28 mx-8">
       <h1 className="text-3xl text-black font-bold">Province</h1>
+      <div className="grid grid-cols-4 gap-5">
+      <CardCalenderEvents/>
+      <CardCalenderEvents/>
+      <CardCalenderEvents/>
+      <CardCalenderEvents/>
+      </div>
     </div>
   );
 };
