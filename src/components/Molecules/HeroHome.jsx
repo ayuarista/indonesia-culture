85b9617e57a7097617fb3fdc51b3841d31013a49
ayuarista <aristaadewi8@gmail.com>
2025-04-19 22:10:01 +0800

    replace HeroHome image with video for enhanced visual experience

--- src/components/Molecules/HeroHome.jsx
+++ src/components/Molecules/HeroHome.jsx
@@ -1,14 +1,10 @@
-import gelombang from "/assets/home/gelombang-home.svg"
-
+import gelombang from "/assets/home/gelombang-home.svg";
+import HeroVideo from "/assets/home/hero.mp4";
 const HeroHome = () => {
-    return(
-        <div className="relative h-screen">
-            <div className="bg-gradient-to-b from-[#430304] to-[#A90809]  min-h-[40vh]">
-                <h1 className="font-bold text-6xl text-center absolute top-32">Preserve Culture, 
-                Protect National Identity</h1>
-            </div>
-            <img src={gelombang} alt="" className="absolute -mt-1"/>
-           </div>
-    )
-}
+  return (
+    <div className="w-full relative mx-auto">
+      <video src={HeroVideo} className="w-full" autoPlay loop type="video/mp4"></video>
+    </div>
+  );
+};
 export default HeroHome;
