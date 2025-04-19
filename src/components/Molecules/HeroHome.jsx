6aaa45afe2847f52e7a822fe4c7df954dfaf3c04
ayuarista <aristaadewi8@gmail.com>
2025-04-19 22:22:49 +0800

    enhance HeroHome component with video background and overlay text for improved user engagement

--- src/components/Molecules/HeroHome.jsx
+++ src/components/Molecules/HeroHome.jsx
@@ -3,8 +3,24 @@ import HeroVideo from "/assets/home/hero.mp4";
 const HeroHome = () => {
   return (
     <div className="w-full relative mx-auto">
-      <video src={HeroVideo} className="w-full" autoPlay loop type="video/mp4"></video>
-    </div>
+      <video
+        src={HeroVideo}
+        className="w-full h-[500px] object-cover"
+        autoPlay
+        loop
+        muted
+        type="video/mp4"
+      ></video>
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-black/25 text-white px-4">
+    <h1 className="text-4xl md:text-6xl font-third font-bold text-center mb-4">
+      Welcome to Nusantara Culture
+    </h1>
+    <p className="text-base md:text-lg text-center max-w-2xl">
+      Discover the rich heritage, timeless traditions, and vibrant diversity
+      of Indonesia's archipelagic culture.
+    </p>
+  </div>
+</div>
   );
 };
 export default HeroHome;
